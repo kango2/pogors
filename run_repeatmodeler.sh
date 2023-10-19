09420a7256cc7de98f900d457da16ad9c17365cd
J King Chang <105471690+JKing2000@users.noreply.github.com>
2023-10-19 12:25:09 +1100

    Update run_repeatmodeler.sh
    Updated the script so that it does not use scratch space as working directory but instead uses $TMPDIR, it will now rsync the tar-ed working directory over when the job finishes
    
    This is due to the working directory containing lots of small file and filling up inodes

--- run_repeatmodeler.sh
+++ run_repeatmodeler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l ncpus=24,mem=60GB,walltime=48:00:00,storage=gdata/if89+gdata/xl04
+#PBS -l ncpus=24,mem=60GB,walltime=48:00:00,storage=gdata/if89+gdata/xl04,jobfs=400GB
 #PBS -N repeatmodeler
 #PBS -j oe
 
@@ -11,14 +11,15 @@ cd ${workingdir}
 mkdir database
 cd database
 BuildDatabase -name ${species} -engine ncbi ${inputgenome}
-cd ${workingdir}
-RepeatModeler -database ${workingdir}/database/${species} -pa ${PBS_NCPUS} > out.log
+
+mkdir -p $TMPDIR/${species}_RepeatModeler
+cd $TMPDIR/${species}_RepeatModeler
+RepeatModeler -database ${workingdir}/database/${species} -pa ${PBS_NCPUS} > ${workingdir}/out.log
+export RM_folder=$(ls | grep RM_)
+tar cf ${RM_folder} | pigz -p ${PBS_NCPUS} > ${RM_folder}.tar.gz
+rsync ${RM_folder}.tar.gz ${workingdir}
 
 #output repeat library will be in ${workingdir}/database named:
 #${speices}-families.fa
 #${species}-families.stk
 #the .fa file can be fed directory into repeatmasker for soft masking, or you can concatenate it with the taxon repeats library for better repeat coverage
-
-export RM_folder=$(ls | grep RM_) 
-echo -e "cd ${workingdir}\ntar -czvf ${RM_folder}.tar.gz ${RM_folder}\nrm -rf ${RM_folder}" > ${workingdir}/cleanup.sh
-chmod u+x ${workingdir}/cleanup.sh
